8820204efac1a49ca57702b242b5650ddc4bf3ad
Beatriz <beatrizandreosbezerra1@gmail.com>
2026-03-26 18:02:26 -0300

    Feat(Config): Implementando a funcionalidade do 'alias' ao projeto

--- choki-for-you/vite.config.mjs
+++ choki-for-you/vite.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
@@ -17,5 +16,10 @@ export default defineConfig({
     host: "0.0.0.0",
     strictPort: true,
     allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   }
 });
